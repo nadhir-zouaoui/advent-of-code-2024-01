9bdba4de8d0233252f423d785c5b56ea44ad0535
nadhir zouaoui <nadhirzouaoui256@gmail.com>
2024-12-01 19:49:07 +0100

    advent of code day 1 problem 1 my solution

--- adventOfCode/adventOfCode.cpp
+++ adventOfCode/adventOfCode.cpp
@@ -2,8 +2,37 @@
 //
 
 #include <iostream>
-
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream file("input.txt");
+    if (!file.is_open()) {
+        cout << "failed to open" << endl;
+        return 1;
+    }
+    bool target = true;
+    std::vector<int> v1;
+    std::vector<int> v2;
+    string mot;
+    while (file >> mot) {
+        if (target == true) {
+            v1.push_back(stoi(mot));
+        }
+        else {
+            v2.push_back(stoi(mot));
+        }
+        target = !target;
+    }
+    std::sort(v1.begin(),v1.end());
+    std::sort(v2.begin(),v2.end());
+    int s = 0;
+    for (int i = 0;i < v1.size();i++) {
+        s += abs(v1[i] - v2[i]);
+    }
+    cout << s << endl;
+    cout << "Hello World!\n";
 }
