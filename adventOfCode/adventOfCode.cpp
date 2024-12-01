b5dfd67eb6ccacdfabef7b867edc702a7c67ab08
nadhir zouaoui <nadhirzouaoui256@gmail.com>
2024-12-01 21:01:26 +0100

    solution problem2

--- adventOfCode/adventOfCode.cpp
+++ adventOfCode/adventOfCode.cpp
@@ -15,10 +15,19 @@ int main()
         std::cout << "failed to open" << std::endl;
         return 1;
     }
-    std::unordered_map<int, int> map;
+    std::unordered_map<int, int> map2;
     std::string mot;
-    while (getline(file, line)) {
-
+    std::vector<int> v1;
+    while (file >> mot) {
+        v1.push_back(stoi(mot));
+        file >> mot;
+        map2[stoi(mot)] += 1;
     }
+    int s = 0;
+    for (auto& i : v1)
+    {
+        s += i * map2[i];
+    }
+    std::cout << s << std::endl;
     std::cout << "Hello World!\n";
 }
