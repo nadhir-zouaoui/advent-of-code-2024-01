f15198e168d55296e9415ab72d80a461cc79f2e9
nadhir zouaoui <nadhirzouaoui256@gmail.com>
2024-12-01 20:17:41 +0100

    refactor code

--- adventOfCode/adventOfCode.cpp
+++ adventOfCode/adventOfCode.cpp
@@ -6,26 +6,26 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-using namespace std;
+#include <sstream>
+
 int main()
 {
-    ifstream file("input.txt");
+    std::ifstream file("input.txt");
     if (!file.is_open()) {
-        cout << "failed to open" << endl;
+        std::cout << "failed to open" << std::endl;
         return 1;
     }
     bool target = true;
     std::vector<int> v1;
     std::vector<int> v2;
-    string mot;
-    while (file >> mot) {
-        if (target == true) {
-            v1.push_back(stoi(mot));
-        }
-        else {
-            v2.push_back(stoi(mot));
-        }
-        target = !target;
+    std::string line;
+    while (getline(file, line)) {
+        std::istringstream ss(line);
+        std::string mot;
+        ss >> mot;
+        v1.push_back(stoi(mot));
+        ss >> mot;
+        v2.push_back(stoi(mot));
     }
     std::sort(v1.begin(),v1.end());
     std::sort(v2.begin(),v2.end());
@@ -33,6 +33,6 @@ int main()
     for (int i = 0;i < v1.size();i++) {
         s += abs(v1[i] - v2[i]);
     }
-    cout << s << endl;
-    cout << "Hello World!\n";
+    std::cout << s << std::endl;
+    std::cout << "Hello World!\n";
 }
